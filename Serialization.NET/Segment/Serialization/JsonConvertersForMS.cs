2f4deb312aa34bc8be94bef31efaa7da99f6561f
Wenxi Zeng <wzeng@twilio.com>
2025-12-03 16:00:14 -0600

    bug fix

--- Serialization.NET/Segment/Serialization/JsonConvertersForMS.cs
+++ Serialization.NET/Segment/Serialization/JsonConvertersForMS.cs
@@ -14,6 +14,7 @@ namespace Segment.Serialization
         {
             if (value == null)
             {
+                writer.WriteNullValue();
                 return;
             }
 
@@ -44,6 +45,7 @@ namespace Segment.Serialization
         {
             if (value == null)
             {
+                writer.WriteNullValue();
                 return;
             }
             writer.WriteRawValue(value.ToString());
@@ -97,6 +99,7 @@ namespace Segment.Serialization
         {
             if (value == null)
             {
+                writer.WriteNullValue();
                 return;
             }
             writer.WriteRawValue(value.ToString());
