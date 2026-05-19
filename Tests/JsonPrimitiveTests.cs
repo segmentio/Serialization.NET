26d7f8c1fe3c7a7b3c7f667dad1d7a6f9dcf64b1
Gustavo Santos <gustappsantos@gmail.com>
2026-05-19 14:25:46 -0300

    Add JsonPrimitiveTests
    At this commit the tests are still failing, the fix will come later

---
+++ Tests/JsonPrimitiveTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Segment.Serialization;
+using Xunit;
+
+namespace Tests
+{
+    public class JsonPrimitiveTests
+    {
+        [Fact]
+        public void EnsureIntAreNotSerializedWithScientificNotation()
+        {
+            var dict =  new Dictionary<string, object>
+            {
+                {"IntNumber", int.MaxValue.ToString()}
+            };
+
+            var serialized = JsonUtility.ToJson(dict); // This call exist inside Segment.Analytics.Analytics::Track<T>
+            var deserialized = JsonUtility.FromJson<JsonObject>(serialized); // This call exist inside Segment.Analytics.Analytics::Track<T>
+            Assert.Equal(expected: int.MaxValue.ToString(), actual: deserialized.GetString("IntNumber"));
+        }
+
+        [Fact]
+        public void EnsureLongAreNotSerializedWithScientificNotation()
+        {
+            var dict =  new Dictionary<string, object>
+            {
+                {"LongNumber", long.MaxValue.ToString()}
+            };
+
+            var serialized = JsonUtility.ToJson(dict); // This call exist inside Segment.Analytics.Analytics::Track<T>
+            var deserialized = JsonUtility.FromJson<JsonObject>(serialized); // This call exist inside Segment.Analytics.Analytics::Track<T>
+            Assert.Equal(expected: long.MaxValue.ToString(), actual: deserialized.GetString("LongNumber"));
+        }
+    }
+}
